f23d6d0adc805febe4acee7ab03103d0c45aae86
MauriceLee <ee.maurice.lee@gmail.com>
2019-03-10 09:28:31 +0800

    Add os.path.join method

--- Image.py
+++ Image.py
@@ -3,6 +3,6 @@ import os
 
 for file in os.listdir('orig'):
     if file.endswith('.jpg'):
-        image_file = Image.open('orig/' + file)
+        image_file = Image.open(os.path.join('orig', file))
         image_file = image_file.convert('L')
-        image_file.save('result/' + file[:-4] + '_gray.png')
+        image_file.save(os.path.join('result', file[:-4] + '_gray.png'))
